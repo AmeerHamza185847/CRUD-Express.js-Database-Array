6d876ddbff24db32eff84485620544eeb3235207
AmeerHamza185847 <hamzaraza185847@gmail.com>
2024-09-26 05:56:04 -0700

    Users array created

--- server.mjs
+++ server.mjs
@@ -3,7 +3,23 @@ const PORT = 8000;
 
 const app = express();
 
-
+const users = [
+    {
+        id: 1,
+        name: 'Ameer Hamza',
+        email: 'hamza13@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Salman',
+        email: 'salman@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Muhammad Ali',
+        email: 'muhammad19@gmail.com'
+    },
+]
 
 app.get('', (req, res) => {
     res.status(201).send({Users: users });
