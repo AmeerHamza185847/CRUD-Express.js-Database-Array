c12498b52fd0b53e2e22c0b03951602fd5094f80
AmeerHamza185847 <hamzaraza185847@gmail.com>
2024-09-26 06:17:13 -0700

    Create Middleware for JSON

--- server.mjs
+++ server.mjs
@@ -3,6 +3,9 @@ const PORT = 8000;
 
 const app = express();
 
+// middleware for JSON
+app.use(express.json());
+
 const users = [
     {
         id: 1,
@@ -21,8 +24,12 @@ const users = [
     },
 ]
 
-app.get('', (req, res) => {
-    res.status(201).send({Users: users });
+app.get('/', (req, res) => {
+    res.status(201).send({ Users: users });
+})
+app.post('/user', (req, res) => {
+    users.push({ id: users.length + 1, ...req.body });
+    res.status(201).send({message:"User Added Successfully!"})
 })
 
 app.listen(PORT, (req, res) => {
